2a1aafb1c6e89f2e13922d7d94b5e20a0ffff9af
Kehinde Ayeyemi <kenvik446@gmail.com>
2022-07-06 03:00:08 +0000

    initial commit

--- Twitter.py
+++ Twitter.py
@@ -2,14 +2,15 @@ from pytwitter import Api
 from cryptography.x509 import load_pem_x509_certificate
 import pandas as pd
 import sqlalchemy as db
-api = Api(bearer_token= "AAAAAAAAAAAAAAAAAAAAAExweQEAAAAAcHYwAIXpI8VrhLNpc6ELBT%2BOUX0%3DjMNh7b7NgR4e6FPB7LK03O3qHiqeSZ2jtDaylQXvizysxYtnzr" )
-#Getting user ID By username
+BA = "AAAAAAAAAAAAAAAAAAAAAExweQEAAAAAcHYwAIXpI8VrhLNpc6ELBT%2BOUX0%3DjMNh7b7NgR4e6FPB7LK03O3qHiqeSZ2jtDaylQXvizysxYtnzr"
+api = Api(bearer_token= BA)
+# Getting user ID By username
 name=input("Enter username: ")
 s=(api.get_user(username= name))
-#to get just id
+# to get just id
 ID=(s.data.id)  
-#This should return a dictionary of the following
-#The parameters should all be of type list([])
+# This should return a dictionary of the following
+# The parameters should all be of type list([])
 def get_dict(ids, names, usernames) :
     dict = {
         'ids' : ids,
@@ -17,7 +18,6 @@ def get_dict(ids, names, usernames) :
         'usernames' : usernames
         }
     return dict
-
 # This should return a dictionary by calling the get_dict
 # The id must be a of type string and max_users should be of type int 
 def get_users_followers(id, max_users):
@@ -44,7 +44,7 @@ def create_database(info, db_name, table_name):
     query_result = engine.execute("SELECT * FROM " + table_name + ";").fetchall()
     print(pd.DataFrame(query_result), "\n")
 
-  # Getting users followers by ID
+# Getting users followers by ID
 max = input("Enter number of followers you would like to see: ")
 users_followers = get_users_followers(ID, int(max))
 print(users_followers)
